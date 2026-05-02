3a724b99c9ee857a0cf978e95c31fd0a6016f138
henrychinchilla <40329065+henrychinchilla@users.noreply.github.com>
2026-05-02 14:14:44 -0600

    Switch to createBrowserClient for Supabase

--- apps/admin-dashboard/src/lib/supabase.ts
+++ apps/admin-dashboard/src/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = 'https://ryiadkbddanuyjrqzoxn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5aWFka2JkZGFudXlqcnF6b3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NTQxNzgsImV4cCI6MjA5MzIzMDE3OH0.vlN2JPzcvXdV8Zp9E9vBWYCmOKw3PxBJMF3Jvam1UDQ';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
